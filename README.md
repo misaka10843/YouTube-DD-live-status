467206863a91911aedd13f7a0c2a39785ef8b4a4
misaka10843 <misaka10843@outlook.jp>
2022-03-01 09:12:23 +0800

    添加DD.json及相关编写规范，删除多余requirements.txt中的依赖

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # YouTube-DD-live-status
 ## 简介
+
 此仓库是整合了一些YouTube上的虚拟主播的直播状态
 
 暂且准备是每10分钟更新一次
@@ -9,3 +10,46 @@
 您可以在`DD.json`中按照规范来添加你想要的V（虽然还没有实现www）
 
 作者正在全力开发中，还请点个star✨吧qwq
+
+## DD.json编写规范
+
+先看示例json
+
+```json
+{
+  "channel": [
+    {
+      "en": "Kaguranana",
+      "on": "カグラナナ",
+      "cn": "神楽七奈",
+      "isC": 1,
+      "Cid": "かぐらななななかぐ辛党Ch"
+    },
+    {
+      "en": "Shigureui",
+      "on": "しぐれうい",
+      "cn": "时雨羽衣",
+      "isC": 0,
+      "Cid": "UCt30jJgChL8qeT9VPadidSw"
+    }
+  ]
+}
+```
+
+· `en`:代表这个V的名字的罗马音或者英文
+
+· `on`:代表这个V的原名
+
+· `cn`:代表这个V的中文名
+
+· `isC`:因为防止有些V的主页链接不是像`https://www.youtube.com/channel/UCt30jJgChL8qeT9VPadidSw`
+
+这种的所以就需要判断是否是`www.youtube.com/c/かぐらななななかぐ辛党Ch` 这种的
+
+(`c = 1,user = 2,channel = 0`)
+
+· `Cid`:频道的ID
+
+如`www.youtube.com/c/かぐらななななかぐ辛党Ch` 中的`かぐらななななかぐ辛党Ch`
+
+和 `www.youtube.com/channel/UCt30jJgChL8qeT9VPadidSw` 中的`UCt30jJgChL8qeT9VPadidSw`
--- main.py
+++ main.py
@@ -1,20 +1,31 @@
+import json
+
 import requests
 from bs4 import BeautifulSoup
-import json
+
+
+def GetVtb():
+    with open('./DD.json', 'rb') as fp:
+        json_data = json.load(fp)
+    channel = json_data.get('channel')
+    return channel
 
 
 def is_liveYT():
     channel_url = "https://www.youtube.com/c/かぐらななななかぐ辛党Ch/live"
     proxy = '127.0.0.1:8099'
     proxies = {
-        'http': 'http://'+proxy,
-        'https': 'http://'+proxy,
+        'http': 'http://' + proxy,
+        'https': 'http://' + proxy,
     }
     page = requests.get(channel_url, cookies={
-                        'CONSENT': 'YES+42'}, proxies=proxies, timeout=10)
+        'CONSENT': 'YES+42'}, proxies=proxies, timeout=10)
     soup = BeautifulSoup(page.content, "html.parser")
+    # noinspection PyTypeChecker
+    # 禁止idea检查此句防止报错
     live = soup.find("link", {"rel": "canonical"}, {
-                     "href": "https://www.youtube.com/c/かぐらななななかぐ辛党Ch"})
+        "href": "https://www.youtube.com/c/かぐらななななかぐ辛党Ch"})
+    # TODO 输出并组成json
     if not live:
         print("Streaming")
     else:
@@ -22,4 +33,4 @@ def is_liveYT():
 
 
 if __name__ == "__main__":
-    is_liveYT()
+    print(GetVtb())
