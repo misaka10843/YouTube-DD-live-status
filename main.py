802c893c19c0cd20cfc8e3dcac8efffaa30b4198
misaka10843 <misaka10843@outlook.jp>
2022-03-01 20:21:38 +0800

    完成json包装以及输出

--- main.py
+++ main.py
@@ -16,13 +16,8 @@ def GetVtb():
 # 主判断器
 def IsLiveYT(ChannelUrl, ChannelName):
     ChannelLiveUrl = ChannelUrl + "/live"
-    proxy = '127.0.0.1:8099'
-    proxies = {
-        'http': 'http://' + proxy,
-        'https': 'http://' + proxy,
-    }
     page = requests.get(ChannelLiveUrl, cookies={
-        'CONSENT': 'YES+42'}, proxies=proxies, timeout=10)
+        'CONSENT': 'YES+42'}, timeout=10)
     soup = BeautifulSoup(page.content, "html.parser")
 
     # noinspection PyTypeChecker
@@ -50,13 +45,8 @@ def ConversionTime(LiveUrl):
 
 # 获取开播时间(理论)
 def GetWaitTime(LiveUrl):
-    proxy = '127.0.0.1:8099'
-    proxies = {
-        'http': 'http://' + proxy,
-        'https': 'http://' + proxy,
-    }
     page = requests.get(LiveUrl, cookies={
-        'CONSENT': 'YES+42'}, proxies=proxies, timeout=10)
+        'CONSENT': 'YES+42'}, timeout=10)
     soup = BeautifulSoup(page.content, "html.parser")
     # noinspection PyTypeChecker
     # 禁止idea检查此句防止报错
@@ -87,7 +77,8 @@ def main():
                 "cn"] + '","live":false,"start_time":null,"live_url":null},'
     # 出循环，结尾json
     DDJson = DDJson[:-1] + "]}"
-    print(DDJson)
+    with open('./DDLive.json', 'wb') as f:
+        f.write(DDJson.encode())
 
 
 if __name__ == "__main__":
